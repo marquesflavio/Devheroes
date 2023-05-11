2967fbef285916e5e654e7274396555e6e89ec0e
Flávio Marques <flaviomarques2@hotmail.com>
2023-05-11 18:29:08 -0300

    finalizei o style da sessão dos heróis

--- src/components/Heroes/index.js
+++ src/components/Heroes/index.js
@@ -4,7 +4,6 @@ import './Heroes.css'
 const Heroes = (props) => {
     return(
         <div className="heroes">
-
             <label htmlFor='heroes'>{props.label} </label>
             <input id='heroes' value={props.valor} placeholder={props.placeholder} ></input>  
         </div>
