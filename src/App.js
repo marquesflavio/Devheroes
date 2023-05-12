82b03de540e712245c6763675f3407a6d9c5131b
Flávio Marques <flaviomarques2@hotmail.com>
2023-05-11 21:53:58 -0300

    construí a lógica de times e cores dinâmicas com o map para a sessão dos heróis

--- src/App.js
+++ src/App.js
@@ -7,11 +7,50 @@ import Header from "./components/Header";
 
 
 function App() {
+
+  const times =[
+    
+    {
+      classe: 'Bárbaro',
+      corDeFundo: '#8F694F',
+      imagem: '',
+      caracteristica: ''
+    },
+    {
+      classe: 'Bardo',
+      corDeFundo: '#DE4D26',
+      imagem: '',
+      caracteristica: ''
+    },
+    {
+      classe: 'Elfo',
+      corDeFundo: '#8BC34A',
+      imagem: '',
+      caracteristica: ''
+    },
+    {
+      classe: 'Mago',
+      corDeFundo: '#0C95EB',
+      imagem: '',
+      caracteristica: ''
+    },
+    {
+      classe: 'Princesa',
+      corDeFundo: '#E91E63',
+      imagem: '',
+      caracteristica: ''
+    }
+    
+  ]
+
+
   return (
     <div>
       <Header/>
       <Formulario/>
-      <DevHeroes/>
+      {/* Montando o time de forma dinâmica */}
+      {times.map(time =><DevHeroes key={time.classe} classe={time.classe} corDeFundo={time.corDeFundo}/>)}
+
       <Footer/>
     </div>
   );
--- src/components/DevHeroes/index.js
+++ src/components/DevHeroes/index.js
@@ -1,10 +1,11 @@
 import './DevHeroes.css'
 
-const DevHeroes = () => {
+const DevHeroes = (props) => {
     return(
-        <section className='devheroes'>
+        <section className='devheroes' style={{backgroundColor: props.corDeFundo}}>
             <h2>Seus DevHeroes</h2>
             <h3>Nome</h3>
+            <h3>{props.classe}</h3>
             <img src="/images/bardo.png" alt="Imagem do DevHeroe" ></img>
             <p>Frase</p>
 
